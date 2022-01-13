0bcc3aeb1ab007434eecf0e0623b3c87f67176c6
mma37 <mma37@hw.ac.uk>
2022-01-13 12:03:24 +0400

    Made some small changes to the navbar

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -1,36 +1,29 @@
-import { useState, useEffect } from 'react'
+import { useState } from 'react'
 import { Link } from 'react-router-dom'
 import './NavBar.css'
 function NavBar() {
+	// These states help in closing the shadow box
 	const [click, setClick] = useState(false)
 	const handleClick = () => setClick(!click)
 	const closeMobileMenu = () => setClick(false)
-
-	const [button, setButton] = useState(true)
-	const showButton = () => {
-		if (window.innerWidth <= 960) {
-			setButton(false)
-		} else {
-			setButton(true)
-		}
-	}
-	useEffect(() => {
-		showButton()
-	}, [])
-	window.addEventListener('resize', showButton)
 
 	return (
 		<header>
 			<nav className='navbar'>
 				<div className='navbarContainer'>
 					<Link to='/' className='navbarLogo' onClick={closeMobileMenu}>
+						{/* This is the logo with the name of the website, which can be found on the left */}
 						JLMARS <i className='fas fa-globe-europe'></i>
 					</Link>
+
 					<div className='menuIcon' onClick={handleClick}>
+						{/* This is the X and Hamburger logo on the right */}
 						<i className={click ? 'fas fa-times' : 'fas fa-bars'} />
 					</div>
+					{/* This helps to open a shadow box with the nav bar when its a phone or a small display. */}
 					<ul className={click ? 'navMenu active' : 'navMenu'}>
 						<li className={'navItem'}>
+							{/* These are all the items in the nav bar */}
 							<Link to='/' className='navLinks' onClick={closeMobileMenu}>
 								Home
 							</Link>
