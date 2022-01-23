8bcef72651cbb25512b3edebc01c4869c8bab19a
MahmoudAoude <71701098+MahmoudAoude@users.noreply.github.com>
2022-01-23 14:11:56 +0400

    Fixed minor bug were the pages dont scroll up when going to the next one

--- src/index.js
+++ src/index.js
@@ -2,9 +2,11 @@ import React from 'react'
 import ReactDOM from 'react-dom'
 import App from './App'
 import { BrowserRouter } from 'react-router-dom'
+import Scroll from './components/Scroll'
 
 ReactDOM.render(
 	<BrowserRouter>
+		<Scroll />
 		<App />
 	</BrowserRouter>,
 	document.getElementById('root')
