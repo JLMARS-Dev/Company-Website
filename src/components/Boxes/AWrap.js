eb7b5c087b914e0518dac3d52003eafd267a7f9e
MahmoudAoude <mma37@hw.ac.uk>
2022-01-25 15:42:33 +0400

    Added 2 projects.

--- src/components/Boxes/AWrap.js
+++ src/components/Boxes/AWrap.js
@@ -8,12 +8,9 @@ function ABWrap(props) {
 			<div className='boxContainer'>
 				<div className='boxWrapper'>
 					<ul className='boxItem'>
-						<Box src='images/p4.jpg' to='/About' name='Project 1' label='Lead Devleoper' />
-						<Box src='images/p5.jpg' to='/About' name='Project 2' label='Manager' />
-					</ul>
-
-					<ul className='boxItem'>
-						<Box src='images/p5.jpg' to='/About' name='Project 3' label='Back-End Dev' />
+						<Box src='/images/FancySnake.png' to={{pathname:'//github.com/Naughty-Gamer/Fancy-Snake#readme'}} name='Snake Game' label='Dec,2021' />
+						<Box src='images/Pacman.png' to={{pathname:'//github.com/F28WP-Dubai-WPGame/PacMan_Rush#readme'}} name='Pacman' label='Dec,2021' />
+						<Box src='images/p5.jpg' to={{pathname:'//github.com/F28WP-DUBAI-TASKS/Pirate-Lagoon#readme'}} name='Pirate-Lagoon' label='Dec,2021' />
 						<Box src='images/p6.jpg' to='/About' name='Project 4' label='Back-End Dev' />
 					</ul>
 				</div>
