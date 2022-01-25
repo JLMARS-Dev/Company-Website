6242135699889532748135048a973763e7086d1c
MahmoudAoude <71701098+MahmoudAoude@users.noreply.github.com>
2022-01-26 01:20:17 +0400

    Update AboutUs.js

--- src/pages/AboutUs.js
+++ src/pages/AboutUs.js
@@ -11,11 +11,11 @@ function AboutUs() {
 				<div className='text'>
 					<h1>WHO WE ARE</h1>
 					<p>
-						We are a Software Development Company that focuses on developing Web applications, that is responsive for both computers and
-						mobile phones.{<br />} Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the
-						industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a
-						type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining
-						essentially unchanged. It was popularised.
+						We are a Software Development Company that focuses on developing Web applications, which are compatible with both computers and
+						mobile phones.{<br />} We consist of 8 people, which are divided to 2 groups. The Front End team which consists of Mahmoud, Mohammed Sadiq, Lahenshah, 
+						Jishnu and Sherica, this team is always assigend with the design of the applications and the front end development of them. The Back End team consists of
+						Mohammed Sadiq, Svedha, Aresha, Ryan and Mahmoud. They are always working on the backend aspect of the projects, which allows the projects to
+						have functionalities.
 					</p>
 				</div>
 			</Background>
