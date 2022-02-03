6f843689775b3fc58bdb504889f279768b068f9c
MahmoudAoude <71701098+MahmoudAoude@users.noreply.github.com>
2022-02-03 21:47:01 +0400

    added Image to the pirate

--- src/components/Boxes/AWrap.js
+++ src/components/Boxes/AWrap.js
@@ -8,10 +8,25 @@ function ABWrap(props) {
 			<div className='boxContainer'>
 				<div className='boxWrapper'>
 					<ul className='boxItem'>
-						<Box src='/images/FancySnake.png' to={{pathname:'//github.com/Naughty-Gamer/Fancy-Snake#readme'}} name='Snake Game' label='Dec,2020' />
-						<Box src='images/Pacman.png' to={{pathname:'//github.com/F28WP-Dubai-WPGame/PacMan_Rush#readme'}} name='Pacman' label='Dec,2020' />
-						<Box src='images/p5.jpg' to={{pathname:'//github.com/F28WP-DUBAI-TASKS/Pirate-Lagoon#readme'}} name='Pirate-Lagoon' label='Dec,2020' />
-						<Box src='images/read-it.png' to={{pathname:'//localhost:3000'}} name='Read-it' label='Oct,2021' />
+						<Box
+							src='/images/FancySnake.png'
+							to={{ pathname: '//github.com/Naughty-Gamer/Fancy-Snake#readme' }}
+							name='Snake Game'
+							label='Dec,2020'
+						/>
+						<Box
+							src='images/Pacman.png'
+							to={{ pathname: '//github.com/F28WP-Dubai-WPGame/PacMan_Rush#readme' }}
+							name='Pacman'
+							label='Dec,2020'
+						/>
+						<Box
+							src='images/pirate.jpg'
+							to={{ pathname: '//github.com/F28WP-DUBAI-TASKS/Pirate-Lagoon#readme' }}
+							name='Pirate-Lagoon'
+							label='Dec,2020'
+						/>
+						<Box src='images/read-it.png' to={{ pathname: '//localhost:3000' }} name='Read-it' label='Oct,2021' />
 					</ul>
 				</div>
 			</div>
