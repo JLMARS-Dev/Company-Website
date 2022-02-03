1a41ee7407aa5d64d25ae45757f8a1a53a90444c
MahmoudAoude <71701098+MahmoudAoude@users.noreply.github.com>
2022-02-03 11:11:11 +0400

    Added the Read-it project

--- src/components/Boxes/AWrap.js
+++ src/components/Boxes/AWrap.js
@@ -8,10 +8,10 @@ function ABWrap(props) {
 			<div className='boxContainer'>
 				<div className='boxWrapper'>
 					<ul className='boxItem'>
-						<Box src='/images/FancySnake.png' to={{pathname:'//github.com/Naughty-Gamer/Fancy-Snake#readme'}} name='Snake Game' label='Dec,2021' />
-						<Box src='images/Pacman.png' to={{pathname:'//github.com/F28WP-Dubai-WPGame/PacMan_Rush#readme'}} name='Pacman' label='Dec,2021' />
-						<Box src='images/p5.jpg' to={{pathname:'//github.com/F28WP-DUBAI-TASKS/Pirate-Lagoon#readme'}} name='Pirate-Lagoon' label='Dec,2021' />
-						<Box src='images/p6.jpg' to='/About' name='Project 4' label='Back-End Dev' />
+						<Box src='/images/FancySnake.png' to={{pathname:'//github.com/Naughty-Gamer/Fancy-Snake#readme'}} name='Snake Game' label='Dec,2020' />
+						<Box src='images/Pacman.png' to={{pathname:'//github.com/F28WP-Dubai-WPGame/PacMan_Rush#readme'}} name='Pacman' label='Dec,2020' />
+						<Box src='images/p5.jpg' to={{pathname:'//github.com/F28WP-DUBAI-TASKS/Pirate-Lagoon#readme'}} name='Pirate-Lagoon' label='Dec,2020' />
+						<Box src='images/read-it.png' to={{pathname:'//localhost:3000'}} name='Read-it' label='Oct,2021' />
 					</ul>
 				</div>
 			</div>
